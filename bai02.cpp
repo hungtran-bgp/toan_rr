589d772865df9574607110756712a0a99798a691
hungtran_20021371 <duyhungtran716@gmail.com>
2022-03-04 23:47:23 +0700

    Create bai02.cpp

---
+++ bai02.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int arr[] = {1,2,3,4,5,6,7,8,9,10};
+int copysearch(int find) {
+    int l =0, r = 9;
+    while (r > l)
+   {
+        int mid1 = l + (r - l)/3;
+        int mid2 = mid1 + (r - l)/3;
+ 
+        if (arr[mid1] > find) r = mid1;
+        else if (arr[mid2] < find) l = mid2 +1;
+        else {
+            l = mid1;
+            r = mid2;
+        }
+   }
+   if (find == arr[r]) {
+       return r;
+   }
+   return -1;
+}
+int main() {
+    int find;
+    cin>>find;
+    cout << copysearch(find);
+}
